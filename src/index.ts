b2c369e501f95b833b48423fca242c49e2d098ca
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-21 12:12:18 +0530

    implement getPkgManager

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import path from "path";
 import yargs from "yargs";
 import fs from "fs-extra";
 
+import { getPkgManager } from "./helpers/get-pkg-manager";
+
 const args: any = yargs.option({
   name: { type: "string", demandOption: true, alias: "n" },
 }).argv;
@@ -16,16 +18,27 @@ fs.mkdir(`./${name}`, (error: Error) => {
     process.exit(0);
   }
 
+  const pkgManager = getPkgManager();
+
   const srcDir = `${path.resolve(__dirname)}/../template`;
   const destinationDir = `./${name}`;
 
   fs.copySync(srcDir, destinationDir);
 
+  console.log(`using ${pkgManager} to install dependencies`);
+
   console.log(`Created ${name} successfully!`);
   console.log(`Next steps:`);
   console.log(`- cd ${name}`);
-  console.log(`- npm install`);
-  console.log(`- npm run dev`);
+  console.log(`- ${pkgManager} install`);
+
+  if (pkgManager === "yarn") {
+    console.log(`- yarn dev`);
+  } else if (pkgManager === "pnpm") {
+    console.log(`- pnpm dev`);
+  } else {
+    console.log(`- npm run dev`);
+  }
 
   process.exit(0);
 });
